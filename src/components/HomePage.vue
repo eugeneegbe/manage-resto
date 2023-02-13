9523a87f6977c31d81d29da2a60cd57f627f1b47
eugene233 <agboreugene@gmail.com>
2023-02-13 10:47:02 +0100

    Displayed user name from data from local storage on home

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,9 +1,15 @@
 <template>
-    <h1>Hello user, Welcome on Home Page</h1>
+    <h1>Hello {{name}}, Welcome on Home Page</h1>
 </template>
 
 <script>
+    let username = JSON.parse( localStorage.getItem( 'user-data' ) ).name
     export default {
         name: "HomePage",
+        data() {
+            return {
+                name: username
+            }
+        }
     }
 </script>
