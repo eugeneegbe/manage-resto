bd3dd9e1af80862f9fc07b86b1233b8d3ad2c00c
eugene233 <agboreugene@gmail.com>
2023-02-13 13:30:13 +0100

    Added sign up button to home

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,19 +1,15 @@
 <template>
     <h1>Hello {{name}}, Welcome on Home Page</h1>
+    <button @click="$router.push('/sign-up')"> Sign Up</button>
 </template>
 
 <script>
-    let username = JSON.parse( localStorage.getItem( 'user-data' ) ).name
+    let username_data = JSON.parse( localStorage.getItem( 'user-data' ) )
     export default {
         name: "HomePage",
         data() {
             return {
-                name: username
-            }
-        },
-        mounted(){
-            if(!this.username){
-                this.$router.push({name: 'SignUp'})
+                name: username_data ? username_data.name : "Visitor"
             }
         }
     }
