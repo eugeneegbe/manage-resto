99af86abc322b8ccff23d3ccb274692ed96f46bd
eugene233 <agboreugene@gmail.com>
2023-02-13 10:31:32 +0100

    Introduced  routes to application

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <template>
-  <img class="logo" src="./assets/yarafdmall.jpeg"/>
-  <SignUp />
+  <router-view />
 </template>
 
 <script>
-import SignUp from './components/SignUp.vue';
+
 
 export default {
-  name: 'App',
-  components: {
-    SignUp
-  }
+  name: 'App'
 }
 </script>
 
