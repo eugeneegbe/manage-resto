d7766d94437554da80289ab83c3618ca0e42fd3e
eugene233 <agboreugene@gmail.com>
2023-02-19 20:41:33 +0100

    Added product listing

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,6 +2,20 @@
     <ResHeader />
     <img class="logo" src="../assets/yarafdmall.jpeg" />
     <h1>Hello {{ name }}, Welcome on Home Page</h1>
+    <table border="1">
+        <tr>
+            <td>No</td>
+            <td>Name</td>
+            <td>Amount In Stock</td>
+            <td>Unit Price</td>
+        </tr>
+        <tr v-for="item in restaurants" :key="item.id">
+            <td>{{item.id}}</td>
+            <td>{{item.name}}</td>
+            <td>{{item.stock}}</td>
+            <td>{{item.price}}</td>
+        </tr>
+    </table>
 </template>
 
 <script>
@@ -15,7 +29,8 @@ export default {
     },
     data() {
         return {
-            name: ''
+            name: '',
+            restaurants: []
         }
     },
     mounted() {
@@ -23,6 +38,27 @@ export default {
 
         if(user_data){
             this.name = user_data.name
+            let restaurants = [
+                {
+                    "id": 1,
+                    "name": "Bavaria",
+                    "stock": 25,
+                    "price": 1000
+                },
+                {
+                    "id": 2,
+                    "name": "Tour Cantelou",
+                    "stock": 400,
+                    "price": 5000
+                },
+                {
+                    "id": 3,
+                    "name": "Djino Cocktail",
+                    "stock": 30,
+                    "price": 300
+                }
+            ]
+            this.restaurants = restaurants
         }else{
             this.$router.push({ path: '/login' })
         }
@@ -33,5 +69,10 @@ export default {
 <style>
 .logo {
     width: 100px;
+}
+td{
+    widows: 160px;
+    height: 40px;
+    padding: 2px;
 }
 </style>
