c617570808abfb13535220d8ad00c81dc93e45dd
eugene233 <agboreugene@gmail.com>
2023-02-20 12:39:31 +0100

    Added TODO sections for completion for add-update

--- src/components/UpdateProduct.vue
+++ src/components/UpdateProduct.vue
@@ -9,7 +9,7 @@
         <input type="number" name="stock" v-model="product.stock"/>
         <label>Unit Price:</label>
         <input type="number" name="price"  v-model="product.price"/>
-        <button type="button" v-on:click="addProduct">Update Product</button>
+        <button type="button" v-on:click="updateProduct">Update</button>
     </form>
 </template>
 
@@ -30,14 +30,33 @@ export default {
             }
         }
     },
+    methods: {
+        updateProduct(){
+            // Make a request to the server to update product with id from route params
+            // then redirect to the home page
+        }
+    },
     mounted() {
         let user_data = JSON.parse(localStorage.getItem('user-data'))
 
         if(user_data){
-            this.name = user_data.name
+            console.log(this.$route.params.id)
+            /* Use the above id parameter, make a request to the  server
+            get the data return and set the data value for product
+            This should preset the fields
+            like so:
+
+            this.product.name = "Cassava"
+            this.product.stock = 300;
+            this.product.price = 1000; 
+            
+            NOTE: with binding, this.restaurant = result.data
+            */
+
         }else{
             this.$router.push({ path: '/login' })
         }
+
     }
 }
 </script>
